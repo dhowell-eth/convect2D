a233ef8c2d041777a67cc55e0d6aaf5f3a8decd6
Dorran Howell <dorranhowell@public-docking-nw-2649.ethz.ch>
2016-12-06 13:05:51 +0100

    Added final plots for turning in assignment. Also modified the matlab script to have an overall title on the output video using the suptitle command

--- updated_plot_script.m
+++ updated_plot_script.m
@@ -1,7 +1,17 @@
 % Remember full file needs to be transposed once subsetted
+
+%%%%%
+out_name = 'conv_movie_5.avi';
+frame_rate = 2;
+main_title = 'Pr=0.1; Ra=1e7';
+%%%%%
+
+
 full = load('5/T_field.dat');
 full_phi = load('5/phi_field.dat');
 full_w = load('5/w_field.dat');
+
+
 
 times = unique(full(:,1));
 
@@ -10,11 +20,21 @@ full_data = full(:,2:end);
 full_data_phi = full_phi(:,2:end);
 full_data_w = full_w(:,2:end);
 
-figure(1)
+fig1=figure(1);
+
+title('Hey there')
 h1 = subplot(3,1,1);
 h2 = subplot(3,1,2);
 h3 = subplot(3,1,3);
+suptitle(main_title);
 %% 
+
+ 
+rect = get(gcf,'Position');
+rect(1:2) = [0 0];
+numframes = length(times);
+A=moviein(numframes,fig1,rect);
+
 for i=1:length(times)
    this_time = times(i);
    test = find(data_times==times(i));
@@ -28,6 +48,12 @@ for i=1:length(times)
    title(h2,strcat('Omega: t= ',num2str(this_time)));
    contourf(h3,this_data_phi);
    title(h3,strcat('Stream Function: t= ',num2str(this_time)));
-   pause(1.5);
+   A(:,i) = getframe(fig1,rect); %Rest of Frames
+   
 end
 
+writerObj = VideoWriter(out_name); % Name it.
+writerObj.FrameRate = frame_rate; % How many frames per second.
+open(writerObj) %Open your video file
+writeVideo(writerObj,A) %Write your video matrix to your avi file
+close(writerObj)
